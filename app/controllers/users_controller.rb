4893b79b4daaf67b01897724510d2cfb8b988fd0
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2022-11-06 15:01:49 -0300

    Method create in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to "/posts"
+    else
+      render :action => "new"
+    end
+  end
+
 end
