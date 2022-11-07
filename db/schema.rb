e503b76bb07dc86f9a6bace89e56f2c151baf4cf
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2022-11-06 21:03:37 -0300

    Hashing password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_06_154637) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_06_235521) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_06_154637) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
