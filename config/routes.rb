119b912917a04d006dfb2f153897a89e4457a277
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2022-11-07 17:06:19 -0300

    Routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show, :new, :create, :destroy]
   resources :posts, only: [:index, :show, :new, :create, :destroy]
+  resources :comments, only: [:new, :create, :destroy]
   
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
