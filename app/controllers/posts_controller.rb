6fe3e1065a74c1bc70a68953d28326c557c5be32
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2022-11-07 10:48:29 -0300

    Create posts
    (I'm using arbitrary user_id at the moment, change this later)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,22 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = 4
+    if @post.save
+      redirect_to "/posts"
+    else
+      render "new"
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :post_id)
   end
 
 end
