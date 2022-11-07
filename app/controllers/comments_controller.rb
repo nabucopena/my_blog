d4b62d4700dbe289c73600fd660c8259f4035ee5
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2022-11-07 18:52:20 -0300

    Create and show comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,17 @@
 class CommentsController < ApplicationController
 
   def new
-    @comment = Comment.new(comment_params)
+    @comment = Comment.new(new_comment_params)
   end
 
   def create
+    @comment = Comment.new(create_comment_params)
+    @comment.user_id = 4
+    if @comment.save
+      redirect_to "/posts/#{@comment.post_id}"
+    else
+      render "new"
+    end
   end
 
   def destroy
@@ -12,8 +19,12 @@ class CommentsController < ApplicationController
 
   private
 
-    def comment_params
+    def new_comment_params
       params.permit(:post_id, :comment_id)
+    end
+
+    def create_comment_params
+      params[:comment].permit(:content, :post_id, :comment_id)
     end
 
 end
