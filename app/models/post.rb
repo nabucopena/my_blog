f5d789f7ea213010c41861eb960f332e5292cf76
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2022-11-08 15:14:17 -0300

    Validations
    - Posts: validate title, content and user
    - Comments: validate content, user and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :user_id, presence: true
 end
